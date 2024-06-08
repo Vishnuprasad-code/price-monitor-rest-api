16d921a6c00778c84096c5cfff5cd25c08f16825
Vishnuprasad-code <vissprasad11@gmail.com>
2024-06-09 00:19:12 +0530

    create test api

--- pm_api/views.py
+++ pm_api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+from pm_api import serializers
+
+
+class HelloApiView(APIView):
+    '''Test API view'''
+
+    serializer_class = serializers.HelloSerializer
+    def get(self, request, format=None):
+        '''Returns a list of APIView features'''
+        an_api_view = [
+            "World"
+        ]
+    
+        return Response(
+            {'message': 'Hello'}
+        )
+
+    def post(self, request):
+        '''create a hello message with name'''
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response(message)
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
